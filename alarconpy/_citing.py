222fe17cfe4f14ea0cac9e92652702eacaec331e
Albenis <67173990+apalarcon@users.noreply.github.com>
2021-04-06 16:08:20 +0200

    Add files via upload

---
+++ alarconpy/_citing.py
@@ -0,0 +1,13 @@
+
+def get_cite(t):
+    """
+    to get cite
+
+    t:str
+    type of cite
+    aviable option: bibtex
+    """
+    if t=="bibtex":
+        cite="@software{alarconpy,author:{Pére-Alarcón, A.  and Fernández-Alvarez, J. C.},title:{alarconpy: A {Python} {P}ackage for Meteorologists},year:{2018-2021},version:{1.0.4},address:{La Habana, Cuba},doi:{10.5281/zenodo.4493257}}"   
+
+    return cite
