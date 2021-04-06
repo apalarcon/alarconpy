130a2c8f872f62a029c17b93c91dd6a166efeebb
Albenis <67173990+apalarcon@users.noreply.github.com>
2021-04-06 16:13:39 +0200

    Update _version.py

--- alarconpy/_version.py
+++ alarconpy/_version.py
@@ -3,11 +3,11 @@ import json
 
 version_json = '''
 {
- "last_update": "2020-02-02",
+ "last_update": "2021-04-06",
  "dirty": false,
  "error": null,
  "contact": "apalarcon1991@gmail.com",
- "version": "1.0.3",
+ "version": "1.0.4",
  "author":"Albenis Pérez Alarcón"
 }
 '''  # END VERSION_JSON
